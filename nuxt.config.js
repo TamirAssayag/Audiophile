7697b769f683b7d3a3176088e852a45d16c2ffdb
Tamir Assayag <tamirassayag@gmail.com>
2021-08-06 01:09:54 +0300

    ssr false

--- nuxt.config.js
+++ nuxt.config.js
@@ -2,8 +2,7 @@ import colors from 'vuetify/es5/util/colors'
 
 export default {
   // Disable server-side rendering: https://go.nuxtjs.dev/ssr-mode
-  ssr: true,
-
+  ssr: false,
   target: 'static',
 
   // Global page headers: https://go.nuxtjs.dev/config-head
