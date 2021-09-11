6ef808921dab9bc0de252b2fbae17643af6890f6
Tamir Assayag <tamirassayag@gmail.com>
2021-09-11 03:31:10 +0300

    rendering client side only category images attempt 2

--- components/Categories/Categories.vue
+++ components/Categories/Categories.vue
@@ -3,8 +3,8 @@
     <div v-for="item in items" :key="item.title">
       <NuxtLink :to="'/' + item.slug">
         <Card :bg-color="'#f1f1f1'">
-          <client-only>
-            <template #image>
+          <template #image>
+            <client-only>
               <NuxtImg
                 provider="static"
                 :src="item.image"
@@ -13,8 +13,8 @@
                 :aria-label="item.title"
                 :title="item.title"
               />
-            </template>
-          </client-only>
+            </client-only>
+          </template>
           <template #title>
             <h4>{{ item.title }}</h4>
           </template>
