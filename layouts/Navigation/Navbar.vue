c491d6a58c7a0b648198a426b0ddf6eebf2c70a0
Tamir Assayag <tamirassayag@gmail.com>
2021-09-20 01:49:36 +0300

    client-only tags in navbar test

--- layouts/Navigation/Navbar.vue
+++ layouts/Navigation/Navbar.vue
@@ -9,9 +9,7 @@
     >
       <v-app-bar absolute color="black" height="80" elevation="2" width="100%">
         <div class="navbar__left">
-          <client-only>
-            <NuxtLink to="/"> <AudiophileLogo /> </NuxtLink>
-          </client-only>
+          <NuxtLink to="/"> <AudiophileLogo /> </NuxtLink>
 
           <ul class="nav__menu">
             <li
@@ -26,83 +24,89 @@
 
         <div class="navbar__right d-flex align-center">
           <v-scroll-y-reverse-transition appear hide-on-leave group>
-            <div v-if="!loggedIn" :key="loggedIn">
-              <NuxtLink
-                class="v-btn btn btn--signup mr-2"
-                title="Sign up"
-                aria-label="Sign up"
-                to="/signup"
-                >Sign Up</NuxtLink
-              >
-              <NuxtLink
-                class="v-btn btn btn--login mr-5"
-                title="Login"
-                aria-label="Login"
-                to="/login"
-                >Login</NuxtLink
-              >
-            </div>
+            <client-only>
+              <div v-if="!loggedIn" :key="loggedIn">
+                <NuxtLink
+                  class="v-btn btn btn--signup mr-2"
+                  title="Sign up"
+                  aria-label="Sign up"
+                  to="/signup"
+                  >Sign Up</NuxtLink
+                >
+                <NuxtLink
+                  class="v-btn btn btn--login mr-5"
+                  title="Login"
+                  aria-label="Login"
+                  to="/login"
+                  >Login</NuxtLink
+                >
+              </div>
+            </client-only>
           </v-scroll-y-reverse-transition>
 
           <v-scroll-y-reverse-transition appear leave-absolute>
-            <div v-if="loggedIn" class="logout mr-5">
-              <v-menu
-                transition="scroll-y-reverse-transition"
-                attach
-                offset-y
-                :top="false"
-              >
-                <template #activator="{ on, attrs }">
-                  <v-btn
-                    v-if="getUser._id"
-                    class="btn btn--account"
-                    elevation="0"
-                    v-bind="attrs"
-                    :aria-label="displayName"
-                    :title="displayName"
-                    v-on="on"
-                  >
-                    {{ displayName }}
-                  </v-btn>
-                </template>
-                <v-list>
-                  <NuxtLink
-                    v-for="(item, index) in userActions"
-                    :key="index"
-                    class="user--aciton"
-                    :to="item.link"
-                  >
-                    <v-list-item>
-                      <v-list-item-title>{{ item.title }} </v-list-item-title>
+            <client-only>
+              <div v-if="loggedIn" class="logout mr-5">
+                <v-menu
+                  transition="scroll-y-reverse-transition"
+                  attach
+                  offset-y
+                  :top="false"
+                >
+                  <template #activator="{ on, attrs }">
+                    <v-btn
+                      v-if="getUser._id"
+                      class="btn btn--account"
+                      elevation="0"
+                      v-bind="attrs"
+                      :aria-label="displayName"
+                      :title="displayName"
+                      v-on="on"
+                    >
+                      {{ displayName }}
+                    </v-btn>
+                  </template>
+                  <v-list>
+                    <NuxtLink
+                      v-for="(item, index) in userActions"
+                      :key="index"
+                      class="user--aciton"
+                      :to="item.link"
+                    >
+                      <v-list-item>
+                        <v-list-item-title>{{ item.title }} </v-list-item-title>
+                      </v-list-item>
+                    </NuxtLink>
+                    <v-list-item style="cursor: pointer">
+                      <v-list-item-title @click="logOut"
+                        >Logout
+                      </v-list-item-title>
                     </v-list-item>
-                  </NuxtLink>
-                  <v-list-item style="cursor: pointer">
-                    <v-list-item-title @click="logOut"
-                      >Logout
-                    </v-list-item-title>
-                  </v-list-item>
-                </v-list>
-              </v-menu>
-            </div>
+                  </v-list>
+                </v-menu>
+              </div>
+            </client-only>
           </v-scroll-y-reverse-transition>
         </div>
 
-        <v-btn
-          :disabled="$route.name !== 'checkout' ? false : true"
-          icon
-          text
-          title="Cart"
-          aria-label="Cart"
-          class="cart__icon"
-          @click="toggleCartDialog"
-        >
-          <CartSvg />
-          <v-slide-x-reverse-transition v-if="cart.length" appear>
-            <div v-if="$route.name !== 'checkout'" class="cart__amount">
-              <h4 class="text--bold text--white">{{ getTotalCartItems }}</h4>
-            </div>
-          </v-slide-x-reverse-transition>
-        </v-btn>
+        <client-only>
+          <v-btn
+            :disabled="$route.name !== 'checkout' ? false : true"
+            icon
+            text
+            title="Cart"
+            aria-label="Cart"
+            class="cart__icon"
+            @click="toggleCartDialog"
+          >
+            <CartSvg />
+            <v-slide-x-reverse-transition v-if="cart.length" appear>
+              <div v-if="$route.name !== 'checkout'" class="cart__amount">
+                <h4 class="text--bold text--white">{{ getTotalCartItems }}</h4>
+              </div>
+            </v-slide-x-reverse-transition>
+          </v-btn>
+        </client-only>
       </v-app-bar>
     </div>
     <div
@@ -110,13 +114,13 @@
       :class="['navbar', { 'z-index--150': $route.name === 'checkout' }]"
     >
       <v-app-bar absolute color="black" height="80" elevation="2" width="100%">
+        <v-btn icon title="Menu" aria-label="Menu" @click="drawer = !drawer">
+          <MenuSvg />
+        </v-btn>
+        <NuxtLink to="/">
+          <AudiophileLogo />
+        </NuxtLink>
         <client-only>
-          <v-btn icon title="Menu" aria-label="Menu" @click="drawer = !drawer">
-            <MenuSvg />
-          </v-btn>
-          <NuxtLink to="/">
-            <AudiophileLogo />
-          </NuxtLink>
           <v-btn
             :disabled="$route.name !== 'checkout' ? false : true"
             icon
@@ -156,32 +160,30 @@
       </div>
       <v-slide-y-transition v-else appear mode="in-out">
         <div class="menu">
-          <client-only>
-            <v-avatar size="40px" color="primary">
-              <p class="text--white text--bold">{{ userCapitalLetters }}</p>
-            </v-avatar>
-            <div class="menu__actions">
-              <v-btn
-                v-for="(item, index) in userActions"
-                :key="index"
-                elevation="0"
-                class="btn btn--border"
-                title="Logout"
-                aria-label="Logout"
-                :to="item.link"
-              >
-                {{ item.title }}
-              </v-btn>
-            </div>
+          <v-avatar size="40px" color="primary">
+            <p class="text--white text--bold">{{ userCapitalLetters }}</p>
+          </v-avatar>
+          <div class="menu__actions">
             <v-btn
-              class="btn btn--text"
+              v-for="(item, index) in userActions"
+              :key="index"
+              elevation="0"
+              class="btn btn--border"
               title="Logout"
               aria-label="Logout"
-              @click="logOut"
+              :to="item.link"
             >
-              Logout
+              {{ item.title }}
             </v-btn>
-          </client-only>
+          </div>
+          <v-btn
+            class="btn btn--text"
+            title="Logout"
+            aria-label="Logout"
+            @click="logOut"
+          >
+            Logout
+          </v-btn>
         </div>
       </v-slide-y-transition>
       <div class="mobile__menu">
